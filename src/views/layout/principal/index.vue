b3d75039bafe916c5bd58de26f6e9765e196ac50
Daniel <danikzero@hotmail.com>
2024-03-21 10:09:24 -0300

    :recycle: refactor: Refatoração do menu

--- src/views/layout/principal/index.vue
+++ src/views/layout/principal/index.vue
@@ -9,45 +9,21 @@
         </div>
 
         <div class="drawer-side z-30">
-            <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay">
-            </label>
-            <ul class="menu bg-base-100 min-h-full w-56" id="menu">
-                <logoGestor :G="fillG" :ESTOR="fillESTOR" class="p-4" />
-                <li>
-                    <router-link to="/principal" active-class="active"><i class="bx bx-home"></i>Início</router-link>
-                </li>
-                <li>
-                    <details>
-                        <summary><i class="bx bx-basket"></i>Pedidos</summary>
-                        <ul>
-                            <li><router-link to="/pedidos" active-class="active">Meus pedidos</router-link></li>
-                            <li>
-                                <details>
-                                    <summary><i class="bx bx-chart"></i>Relatórios</summary>
-                                    <ul>
-                                        <li><router-link to="/relatorio/bling" active-class="active">Bling</router-link></li>
-                                        <li><router-link to="/relatorio/proporcao" active-class="active">Proporção</router-link></li>
-                                    </ul>
-                                </details>
-                            </li>
-                        </ul>
-                    </details>
-                </li>
-            </ul>
+            <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+            <menuLateral :fillG="fillG" :fillESTOR="fillESTOR"></menuLateral>
         </div>
     </div>
 </template>
 <script lang="ts">
-import logoGestor from '../../../components/logo/index.vue';
 import footerGestor from '../../../components/footer/index.vue';
 import navbarGestor from '../../../components/navbar/index.vue';
-
+import menuLateral from '../../../components/menu/menu.vue';
 export default {
     name: 'principal',
     components: { 
-        logoGestor, 
         footerGestor, 
-        navbarGestor
+        navbarGestor,
+        menuLateral
     },
     data() { 
         return { 
